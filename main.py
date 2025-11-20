507de94ec1319c94ed26f22221199143914cf210
xmdjy <1833299761@qq.com>
2025-11-20 13:58:45 +0800

    优化prompt和推送内容组织

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ password=config['email']['password']
 to_addr=config['email']['to_addr']
 query_content=config['papaers']['query_content']
 max_results=config['papaers']['max_results']
-tar=get_papers(query_content,max_results)
-s=ai_summarize(tar)
+paper_list=tar=get_papers(query_content,max_results)
+
 
 def job():
-    send_email(to_addr,s,port,server,sender,password)
+    all_contents=[]
+    for p in paper_list:
+        res=ai_summarize(p)
+        all_contents.append(res)
+        time.sleep(1)
+    email_body = "\n\n---\n\n".join(all_contents)
+    day=date.today()
+    email_header=f"您好,请查收{day}的论文推送,祝您愉快!"
+    final_content = email_header + "\n\n" + email_body
+    send_email(to_addr,final_content,port,server,sender,password)
 
 if __name__ == "__main__":
-    schedule.every().day.at("09:00").do(job)
+    schedule.every().day.at("14:15").do(job)
     while True:
         schedule.run_pending()
         time.sleep(1)
